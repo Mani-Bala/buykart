b077de864c27aef1f3ee1c69c07bd825ebc415eb
Manikandan007 <58414757+Manikandan007@users.noreply.github.com>
2020-06-24 00:03:54 +0530

    initial commit

---
+++ src/main/java/com/dream/buykart/BuykartApplication.java
@@ -0,0 +1,13 @@
+package com.dream.buykart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuykartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuykartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/buykart/BuykartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.buykart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuykartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
