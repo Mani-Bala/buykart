1de3fa095dbfbe770b0c113a0cd8688b6d4f4299
Mani-Bala <52503735+Mani-Bala@users.noreply.github.com>
2020-06-24 19:19:34 +0530

    Update BuykartApplicationTests.java

--- src/test/java/com/dream/buykart/BuykartApplicationTests.java
+++ src/test/java/com/dream/buykart/BuykartApplicationTests.java
@@ -2,6 +2,7 @@ package com.dream.buykart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig
 class BuykartApplicationTests {
