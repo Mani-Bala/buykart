0c791e018513411785b4d4063c15d13351923688
Mani-Bala <52503735+Mani-Bala@users.noreply.github.com>
2020-06-24 19:15:37 +0530

    Annotation changes for test app

--- src/test/java/com/dream/buykart/BuykartApplicationTests.java
+++ src/test/java/com/dream/buykart/BuykartApplicationTests.java
@@ -3,7 +3,7 @@ package com.dream.buykart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringJUnitConfig
 class BuykartApplicationTests {
 
 	@Test
